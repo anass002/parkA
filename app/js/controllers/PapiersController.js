ae6c3e93ef4e56c2b936cca5a79c59d707716607
Ubuntu <ubuntu@ip-172-31-63-80.ec2.internal>
2017-03-05 18:13:36 +0000

    export button + other issues

--- app/js/controllers/PapiersController.js
+++ app/js/controllers/PapiersController.js
@@ -88,6 +88,7 @@ angular.module('MetronicApp').controller('PapiersController', function($rootScop
 		$scope.data.hideDivFormPapiers = false;
 		$scope.data.newPaperAdded = false;
 		$scope.data.errorAddNewPaper = false;
+        $scope.data.hideDivInfosCar = true;   
     }
 
     $scope.closeInfosCar = function(){
